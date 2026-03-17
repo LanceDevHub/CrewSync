4b65ebf0548e5a0797e4440a8e3af0d9b1ab26cc
Leonard <opticpeak@gmail.com>
2026-03-17 21:14:51 +0100

    first user schema test

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,13 @@ from fastapi import FastAPI
 from app.core.config import settings
 from app.core.database import Base, engine
 from app.models import User, Event, EventParticipant
+from app.schemas import UserCreate
 
 app = FastAPI(title=settings.app_name)
 
+@app.post("/test-user")
+def test_user(user: UserCreate):
+    return user
 
 @app.get("/")
 def root():
