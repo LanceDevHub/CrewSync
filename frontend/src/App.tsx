04751a0cf5d7b30f42cfb439f6f93eec0a626318
Leonard <opticpeak@gmail.com>
2026-03-20 16:05:54 +0100

    /login initial route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -73,7 +73,7 @@ export default function App() {
       {authError && <p style={{ color: "red" }}>{authError}</p>}
 
       <Routes>
-        <Route path="/" element={<Navigate to="/events" replace />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route
           path="/login"
           element={<LoginPage onLoginSuccess={setCurrentUser} />}
