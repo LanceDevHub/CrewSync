09674027ede523953f95189c131a617e68599df6
Leonard <opticpeak@gmail.com>
2026-03-26 18:00:37 +0100

    trying to connect to backend

--- frontend/src/lib/api-client.ts
+++ frontend/src/lib/api-client.ts
@@ -1,5 +1,4 @@
-const API_BASE_URL = "http://localhost:8000";
-
+const API_BASE_URL = "https://crewsync-f5vv.onrender.com"
 type RequestOptions = Omit<RequestInit, "body"> & {
   body?: unknown;
 };
